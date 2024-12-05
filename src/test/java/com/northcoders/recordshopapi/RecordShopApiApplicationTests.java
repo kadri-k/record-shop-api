9524d49f34a67b5b48fb7fe48140e51aef8089a4
kadri-k <kklampe@gmail.com>
2024-12-05 12:41:13 +0000

    create dev properties file and prod properties file for each environment

---
+++ src/test/java/com/northcoders/recordshopapi/RecordShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
