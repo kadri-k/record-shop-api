03b1803035f3492dde212cff95ad8b917f0f1a3d
kadri-k <kklampe@gmail.com>
2024-12-06 11:52:46 +0000

    feat: implemented healthcheckcontroller with endpoint for health checks

--- src/main/java/com/northcoders/recordshopapi/RecordShopApiApplication.java
+++ src/main/java/com/northcoders/recordshopapi/RecordShopApiApplication.java
@@ -2,8 +2,10 @@ package com.northcoders.recordshopapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.northcoders.recordshopapi")
 public class RecordShopApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/northcoders/recordshopapi/controller/HealthCheckController.java
@@ -0,0 +1,18 @@
+package com.northcoders.recordshopapi.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> checkHealth() {
+        return new ResponseEntity<>("Application is working", HttpStatus.OK);
+    }
+}
